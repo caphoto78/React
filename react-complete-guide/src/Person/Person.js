111e9a6ca402c1ca099d70f23eecccac0d796cfe
Constantin Antochi <constantin.antochi@gmail.com>
2021-04-08 14:35:01 +0300

    state

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export const Person = () => {
+const Person = (props) => {
   return (
     <div>
-      
+    <p>I'm {props.name} and I am {props.age}!</p>
+    <p>{ props.children }</p>
     </div>
   )
 }
+
+export default Person
+
+
