8e43084605a0056655f171757e49e675849d3776
Constantin Antochi <constantin.antochi@gmail.com>
2021-04-08 16:23:03 +0300

    useState update

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -27,6 +27,7 @@ const App = props => {
       ],
       // otherState: personsState.otherState
    } );
+   setOtherState('just another value');
   }
  
   return (
@@ -34,9 +35,19 @@ const App = props => {
       <h1>Hi, I'm a React App</h1>
       <p>This is really working</p>
       <button onClick={switchNameHandler} >Switch Name</button>
-      <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age } />
-      <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age } > My hobbies: Swimming </Person>
-      <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age } />
+      <Person
+        name={ personsState.persons[0].name }
+        age={ personsState.persons[0].age } />
+      <Person
+        name={ personsState.persons[1].name } 
+        age={ personsState.persons[1].age }
+      > 
+          My hobbies: Swimming
+      </Person>
+      <Person
+        name={ personsState.persons[2].name }
+        age={ personsState.persons[2].age } 
+      />
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hi, I\'m a React App!!!'));
