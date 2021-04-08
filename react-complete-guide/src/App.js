bac3ec344a35fd08b36adcfe2dccae1c5ecf187a
Constantin Antochi <constantin.antochi@gmail.com>
2021-04-08 15:11:00 +0300

    set state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,7 +8,17 @@ class App extends Component {
       { name: 'Tic', age: 42 },
       { name:'Carmen', age: 41 },
       { name: 'Vladimir', age: 12 }
-    ]
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!')
+    this.setState({ persons: [
+      { name: 'Constantin', age: 42 },
+      { name:'Florenta', age: 41 },
+      { name: 'Vladimir', age: 12 }
+    ] } );
   }
 
   render() {
@@ -16,7 +26,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler} >Switch Name</button>
         <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
         <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } > My hobbies: Swimming </Person>
         <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
