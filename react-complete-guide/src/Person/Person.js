de9c07310d9a7a9055f1e2d0b73cff149a52f883
Constantin Antochi <constantin.antochi@gmail.com>
2021-04-10 12:32:03 +0300

    styled-component finished

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,18 +1,34 @@
 import React from 'react'
-import './Person.css'
+import styled from 'styled-components'
+
+// import './Person.css'
+
+const StyledDiv = styled.div`
+width: 60%;
+margin: 16px auto;
+border: 1px solid #eee;
+box-shadow: 0 2px 3px #ccc;
+padding: 16px;
+text-align: center;
+@media (min-width: 500px) {
+  width: 450px;
+}
+`
 
 const Person = (props) => {
-  const style = {
-    '@media (min-width: 500px)': {
-      width: '450px'
-    }
-  }
+  // const style = {
+  //   '@media (min-width: 500px)': {
+  //     width: '450px'
+  //   }
+  // }
   return (
-    <div className="Person" style={style}>
+    // <div className="Person" style={style}>
+    <StyledDiv>
       <p onClick={props.click}>I'm {props.name} and I am {props.age}!</p>
       <p>{ props.children }</p>
       <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+    </StyledDiv>
+    // </div>
 
   )
 }
