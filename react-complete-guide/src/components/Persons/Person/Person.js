265369c1f2aff77a66dd6cb431c5fd2087cb60f3
Constantin Antochi <constantin.antochi@gmail.com>
2021-04-12 16:27:10 +0300

    refs class based components

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,24 +1,46 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
 // import Aux from '../../../hoc/Aux';
-import classes from './Person.css'
-import withClass from '../../../hoc/withClass'
+import classes from './Person.css';
+import withClass from '../../../hoc/withClass';
+import PropTypes from 'prop-types';
 
 
 class Person extends Component {
+  constructor(props) {
+    super(props)
+    this.inputElementRef = React.createRef()
+  }
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
+
     console.log('[Person.js] rendering...');
     return (
 
       <Fragment>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age}!</p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
+        <input
+          type="text"
+          // ref={(inputEl) => { this.inputElement = inputEl }}
+          ref={this.inputElementRef}
+          onChange={this.props.changed}
+          value={this.props.name} />
       </Fragment>
 
     );
   }
-
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  change: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
 
