4283a043db8627bcbd467c0d400d8b60ec92c683
Constantin Antochi <constantin.antochi@gmail.com>
2021-04-13 19:34:32 +0300

    responsivness

--- burger-builder-starting-setup/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder-starting-setup/src/components/Navigation/Toolbar/Toolbar.js
@@ -6,8 +6,10 @@ import NavigationItems from '../NavigationItems/NavigationItems'
 const Toolbar = props => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
-    <nav>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
       <NavigationItems></NavigationItems>
     </nav>
   </header>
