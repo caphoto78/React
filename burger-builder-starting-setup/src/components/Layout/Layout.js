950802fba75424443517136d3b3c09148f24cad6
Constantin Antochi <constantin.antochi@gmail.com>
2021-04-13 18:35:51 +0300

    toolbar

--- burger-builder-starting-setup/src/components/Layout/Layout.js
+++ burger-builder-starting-setup/src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const Layout = (props) => {
   return (
     <Aux>
-      <div>Toolbar, SideDrawer, Backdrop</div>
+      <Toolbar />
       <main className={classes.Content}>
         {props.children}
       </main>
