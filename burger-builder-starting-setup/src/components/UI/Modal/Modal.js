fbe5ab5d9d9fee376d081f407a3edc5de2d09f45
Constantin Antochi <constantin.antochi@gmail.com>
2021-04-14 10:27:37 +0300

    optimizing

--- burger-builder-starting-setup/src/components/UI/Modal/Modal.js
+++ burger-builder-starting-setup/src/components/UI/Modal/Modal.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import classes from './Modal.css'
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
 
-const Modal = (props) => (
-  <Aux>
-    <Backdrop
-      show={props.show}
-      clicked={props.modalClosed}
-    ></Backdrop>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-)
-export default Modal
+const Modal = (props) => {
+
+  return (
+
+    <Aux>
+      <Backdrop
+        show={props.show}
+        clicked={props.modalClosed}
+      ></Backdrop>
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0'
+        }}
+      >
+        {props.children}
+      </div>
+    </ Aux>
+  )
+}
+export default React.memo(Modal)
