34bd3a3507bd771093e78c1d491fb861abdccbba
Constantin Antochi <constantin.antochi@gmail.com>
2021-04-26 14:33:09 +0300

    updt

--- starting_setup/src/components/ExpenseItem/ExpenseItem.js
+++ starting_setup/src/components/ExpenseItem/ExpenseItem.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from './ExpenseItem.module.css'
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+  const expenseDate = new Date(2021, 2, 28)
+  console.log(expenseDate)
+
+
   return (
-    <div>
-      <h2>Expense Item</h2>
+    <div className={styles.ExpenseItem}>
+      <div>{props.date.toISOString()}</div>
+      <div className={styles.ExpenseItem__description}>
+        <h2>{props.title}</h2>
+        <div className={styles.ExpenseItem__price}>{props.amount}</div>
+      </div>
     </div>
   )
 }
